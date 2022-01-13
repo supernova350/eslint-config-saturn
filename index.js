1ecd159065d69ea969e38bf2d44c4cc8a1e29776
supernova350 <83241467+supernova350@users.noreply.github.com>
2022-01-13 13:20:36 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+module.exports = {
+	rules: {
+		root: true,
+		parser: '@typescript-eslint/parser',
+		parserOptions: {
+			project: './tsconfig.json',
+		},
+		plugins: ['@typescript-eslint'],
+		extends: [
+			'eslint:recommended',
+			'plugin:@typescript-eslint/eslint-recommended',
+			'plugin:@typescript-eslint/recommended',
+		],
+	},
+};
